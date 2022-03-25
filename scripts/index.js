70e76c1a5b56a864b122705306992e8714859793
Nemo Cheng <tiny.nemo.fish@gmail.com>
2022-03-25 23:55:16 +0800

    finish loader

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,23 @@
 function preload(){
-  sc1 = new spriteLoader("assets/Scene1/Scene1.json");
+  huninn_font = loadFont("assets/jf-openhuninn-1.1/jf-openhuninn-1.1.ttf");
 }
 function setup() {
+  loader = new spriteLoader(["assets/Scene1/Scene1.json", "assets/Scene2/Scene2.json"]);
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  noStroke();
-  fill(255);
-  textSize(32);
-  textAlign(CENTER);
-  text("Click to start", width/2, height/2);
+  function show_img(){
+    console.log("image_loaded")
+    background(0);
+    noStroke(); 
+    fill(255);
+    textSize(32);
+    textFont(huninn_font)
+    textAlign(CENTER);
+    text("Click to start", width/2, height/2);
+    [w, h] = loader.get_size("Scene1", "cat_small.png");
+    loader.draw_sprite("Scene1", "cat_small.png", 0, 0, w, h);
+  } 
+  loader.load_all(null, show_img);
+  
 }
 
 function draw(){
--- tools/sprite_loader.js
+++ tools/sprite_loader.js
@@ -1,5 +1,5 @@
 
-async function sync_load_image(url){
+async function async_load_image(url){
     // convert p5JS laodImage into async function
     return new Promise(function(resolve, reject){
         function onSuccess(p5Image){
@@ -9,11 +9,9 @@ async function sync_load_image(url){
             console.error("p5 load image fail:", error);
             reject(error);
         }
-        loadImage(url, [onSuccess], [onFailure]);
+        loadImage(url, onSuccess, onFailure);
     });
 } 
-
-
 
 class spriteLoader{
     constructor(sprite_path_list){
@@ -22,20 +20,58 @@ class spriteLoader{
         this.sprite_total = sprite_path_list.length;
         this.json = {}
         this.image = {}
+        this.ready = false
     }
-    async load_sprite(sprite_path){
+    async load_sprite(sprite_path, progress_callback, ready_callback){
         const filename = sprite_path.substring(sprite_path.lastIndexOf('/')+1);
         const scene_name = filename.substring(0, filename.lastIndexOf('.'));
         console.log("parsing sprite: " + filename);
         // fetch json file 
-        this.data[scene_name] = await fetch(sprite_path).then(response => response.json());
+        this.json[scene_name] = await fetch(sprite_path).then(response => response.json());
         // load image 
-        this.image[scene_name] = loadImage(this.data[scene_name].meta.image);
-        
+        const png_url = sprite_path.substring(0, sprite_path.lastIndexOf('.')) + ".png";
+        this.image[scene_name] = await async_load_image(png_url);
         this.sprite_loaded++;
+        if(typeof progress_callback == "function"){
+            progress_callback(this.sprite_loaded / this.sprite_total);
+        }
+        
+        if(this.sprite_loaded == this.sprite_total){
+            this.ready = true;
+            if(typeof ready_callback == "function"){
+                ready_callback();
+            }
+        }
     }
-    load_all(){
+    async load_all(progress_callback, ready_callback){
+        if(typeof progress_callback == "function"){
+            progress_callback(0);
+        }
+        await Promise.all(this.sprite_path_list.map(x=>this.load_sprite(x, progress_callback, ready_callback)));
+    }
+    draw_sprite(scene_name, sprite_name, x, y, w, h){
+        if(!this.ready){console.error("spritesheet accessed before loading complete");return;}
+        const sprite_image = this.image[scene_name];
+        const sprite_frame = this.json[scene_name]["frames"][sprite_name]["frame"];
+        if(!sprite_frame || !sprite_image){console.error("error fetching sprite data: ",scene_name, sprite_name);return;}
+        image(sprite_image, x, y, w, h, sprite_frame["x"], sprite_frame["y"], sprite_frame["w"], sprite_frame["h"]);
+    }
+    get_sprite_image(scene_name, sprite_name, destImage){
+        if(!this.ready){console.error("spritesheet accessed before loading complete");return;}
+        const sprite_image = this.image[scene_name];
+        const sprite_frame = this.json[scene_name]["frames"][sprite_name]["frame"];
 
+        if(!sprite_frame || !sprite_image){console.error("error fetching sprite data: ",scene_name, sprite_name);return;}
+        destImage.copy(sprite_image, sprite_frame["x"], sprite_frame["y"], sprite_frame["w"], sprite_frame["h"], 0, 0, sprite_frame["w"], sprite_frame["h"]);
     }
-    
+    get_size(scene_name, sprite_name){
+        if(!this.ready){console.error("spritesheet accessed before loading complete");return;}
+        console.log(this.json)
+        const sprite_frame = this.json[scene_name]["frames"][sprite_name]["frame"];
+        return [sprite_frame["w"], sprite_frame["h"]];
+    }
+    get_animation_frame(scene_name, animation_name){
+        if(!this.ready){console.error("spritesheet accessed before loading complete");return;}
+        return Array.from(this.json[scene_name]["animations"][animation_name])
+    }
 }
