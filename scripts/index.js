4b919f12757af098a735b7db2a51b9d7a615d17f
BO YU, Cheng <nemo1999.eecs06@g2.nctu.edu.tw>
2022-04-14 22:59:18 +0800

    succesfully activate the first scene

--- scripts/index.js
+++ scripts/index.js
@@ -6,41 +6,45 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   sm = new getSceneManager();
-  function make_scene(scene_name="scene1"){
-    loader = new SpriteLoader("assets/Scene1/Scene1.json");    
-    var scene1 = new Scene(scene_name);
-    var bg = new Node("background");
-    bg.draw_self = ()=>{background(0);}
-    var bubble = new Node("Bubble");
-    bubble.state.x = windowWidth/2;
-    bubble.state.y = windowHeight/2;
-    bubble.state.r = 100;
-    bubble.draw_self = function(){
-      console.log("draw bubble", this.state.y);
-      push();
-      fill("red");
-      circle(this.state.x,this.state.y,this.state.r)
-      pop();
-    }
-
-    bubble.update_self=function(deltaT){
-      this.state.y -= deltaT / 10;
-    }
-    
-    scene1.add_child(bubble);
-    //scene1.add_child(bg);
-
-    loader.load().then(function(loader){
-      PubSub.publish(scene_name, "activate");
-      PubSub.publish(scene_name, "show");
-    })
-    console.log(scene1)
-    return scene1;
-  } 
   sm.add_scene(make_scene("scene1"));
 }
 
 function draw(){
-  sm.update()
+  sm.update(deltaTime)
   sm.render()
 }
+
+
+function make_scene(scene_name="scene1"){
+  loader = new SpriteLoader("assets/Scene1/Scene1.json");    
+  var scene1 = new Scene(scene_name);
+  var bg = new Node("background");
+  bg.draw_self = ()=>{background(0);}
+  var bubble = new Node("Bubble");
+  bubble.state.x = windowWidth/2;
+  bubble.state.y = windowHeight/2;
+  bubble.state.r = 100;
+
+  bubble.draw_self = function(){
+    push();
+    fill("red");
+    circle(this.state.x,this.state.y,this.state.r)
+    pop();
+  }
+
+  bubble.update_self=function(deltaT){
+    console.log("update bubble", this.state.y);
+    this.state.y -= deltaT / 10;
+  }
+  
+  scene1.add_child(bg);
+  scene1.add_child(bubble);
+  
+
+  loader.load().then(function(loader){
+    PubSub.publish(scene_name, "activate");
+    PubSub.publish(scene_name, "show");
+  })
+  console.log(scene1)
+  return scene1;
+} 
