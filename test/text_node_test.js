e363c0780487b2f39339eb5907bf3750d3f3c63c
Sharpkoi <sharpkoi1222@gmail.com>
2022-10-28 23:48:21 +0800

    style: add name arg for text node

--- test/text_node_test.js
+++ test/text_node_test.js
@@ -24,14 +24,8 @@ function t_font_family(scene_name='font_family') {
         this.show()
     };
 
-    // background
-    // const bg = new Node('background', false);
-    // bg.drawSelf = function() {
-    //     background(200);
-    // };
-
     test_cases.forEach((p, i) => {
-        let text_node = new TextNode(p['content'], true, p['style'])
+        let text_node = new TextNode('text'+i, p['content'], true, p['style'])
         text_node.setTranslate(100, 100*(i+1))
         scene.addChild(text_node)
     });
@@ -43,19 +37,19 @@ function t_text_scaling(scene_name='text_scaling') {
     const test_cases = [
         {
             'content': 'HAHA-GCAT 綠毛蟲要過河',
+            'scale': 1,
             'style': {
-                'font-family': 'openhuninn',
+                'font-family': 'serif',
                 'font-size': 16,
             },
-            'scale': 1
         },
         {
             'content': 'HAHA-GCAT 綠毛蟲要過河',
+            'scale': 2,
             'style': {
                 'font-family': 'openhuninn',
                 'font-size': 16,
             },
-            'scale': 2
         }
     ];
 
@@ -68,7 +62,7 @@ function t_text_scaling(scene_name='text_scaling') {
     };
 
     test_cases.forEach((p, i) => {
-        let text_node = new TextNode(p['content'], true, p['font'], p['font-size'], p['line-height'])
+        let text_node = new TextNode('text'+i, p['content'], true, p['style'])
         text_node.setTranslate(100, 100*(i+1))
         text_node.setScale(p['scale'])
         scene.addChild(text_node)
